d24523ee21b5b49498a8ae28d323aba7e7f6d38d
jsanjeyKumar@gmail.com <jsanjeykumar@gmail.com>
2021-05-13 09:49:08 +0530

    day 3 in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import Checkout from './Checkout'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     //BEM convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Router path="/checkout">
+            <Checkout />
+          </Router>
+          <Router path="/">
+            <Home />
+          </Router>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt="" />
+            </div>
+            <div className="checkout__right"></div>
+        </div>
+    )
+}
+
+export default Checkout
